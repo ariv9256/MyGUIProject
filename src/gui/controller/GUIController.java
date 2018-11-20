4e17a8d5e879174953ccedac8c994b847cd7d28e
Angela Rivas <snowcats001@gmail.com>
2018-11-20 13:16:35 -0700

    Added constructor in collection properly

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,16 +1,19 @@
 package gui.controller;
 
 import gui.model.Cat;
+import gui.view.CatFrame;
 
 public class GUIController
 {
 	private Cat myCat;
+	private CatFrame appFrame;
 	
 	
 	public GUIController()
 	{
 		myCat = new Cat();
 		
+		appFrame = new CatFrame();
 	}
 	public void start()
 	{
