5ea08b5923c6e9d234ef43dd60b575ceea6cdc3d
Angela Rivas <snowcats001@gmail.com>
2018-11-20 13:12:26 -0700

    Added CatPanel class in view and added appController and appPanel specificiations. Also added backgorund color.

--- src/gui/view/CatFrame.java
+++ src/gui/view/CatFrame.java
@@ -1,20 +1,30 @@
 package gui.view;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import gui.controller.GUIController;
 
 public class CatFrame extends JFrame
 {
 	private GUIController appController;
+	private CatPanel appPanel;
 	
 	public CatFrame()
 	{
 		super();
+		this.appController = appController;
+		this.appPanel = new CatPanel(appController);
 		
 		setupFrame();
 	}
 	private void setupFrame()
 	{
-		
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);
+		this.setBackground(Color.red);
+		this.setTitle("There is an invisible cat here, I promise.");
+		this.setResizable(true);
+		this.setVisible(true);
 	}
 }
---
+++ src/gui/view/CatPanel.java
@@ -0,0 +1,33 @@
+package gui.view;
+
+import javax.swing.JPanel;
+import gui.controller.GUIController;
+
+public class CatPanel extends JPanel
+{
+	private GUIController appController;
+	private CatPanel appPanel;
+	
+	public CatPanel(GUIController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+	}
+	private void setupPanel()
+	{
+		
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		
+	}
+}
