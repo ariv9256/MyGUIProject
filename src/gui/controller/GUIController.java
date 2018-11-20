9b09da8ab4df01718ab602fa1b1ac80932312c55
Angela Rivas <snowcats001@gmail.com>
2018-11-20 13:05:57 -0700

    Created CatFrame class in view and constructed controller class and setupFrame method

--- src/gui/controller/GUIController.java
+++ src/gui/controller/GUIController.java
@@ -1,10 +1,15 @@
 package gui.controller;
 
+import gui.model.Cat;
+
 public class GUIController
 {
+	private Cat myCat;
+	
 	
 	public GUIController()
 	{
+		myCat = new Cat();
 		
 	}
 	public void start()
---
+++ src/gui/view/CatFrame.java
@@ -0,0 +1,20 @@
+package gui.view;
+
+import javax.swing.JFrame;
+import gui.controller.GUIController;
+
+public class CatFrame extends JFrame
+{
+	private GUIController appController;
+	
+	public CatFrame()
+	{
+		super();
+		
+		setupFrame();
+	}
+	private void setupFrame()
+	{
+		
+	}
+}
